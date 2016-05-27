7e5a0eeae06ad7ffaaa4635ae15ead3bfd22549a
Jason Knight <jugisahunk@gmail.com>
2016-05-27 15:51:23 -0500

    basic express app setup

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log('Got request for ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>A better, local change in code</h1>');
-}).listen(process.env.PORT);
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+})
